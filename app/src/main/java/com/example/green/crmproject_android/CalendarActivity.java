ec85b34c8d06d4d2835b27e0c9bfc7596ca3ac61
acont42 <48134509+acont42@users.noreply.github.com>
2019-03-25 22:42:05 -0700

    Added some default activities and layouts related to Calendar stuff

--- app/src/main/java/com/example/green/crmproject_android/CalendarActivity.java
+++ app/src/main/java/com/example/green/crmproject_android/CalendarActivity.java
@@ -1,7 +1,10 @@
 package com.example.green.crmproject_android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CalendarActivity extends AppCompatActivity {
 
@@ -9,5 +12,43 @@ public class CalendarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+
+        // Buttons for this screen.
+        Button bDisplayWeek = findViewById(R.id.buttonDisplayWeek);
+        Button bDisplayEvents = findViewById(R.id.buttonDisplayEvents);
+        Button bAddEvent = findViewById(R.id.buttonAddEvent);
+
+        // What the buttons do.
+        bDisplayWeek.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CalendarActivity.this, DisplayWeekActivity.class);
+                //intent.putExtra();
+                startActivity(intent);
+            }
+        });
+
+        bDisplayEvents.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CalendarActivity.this, DisplayEventsActivity.class);
+                //intent.putExtra();
+                startActivity(intent);
+            }
+        });
+
+        bAddEvent.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CalendarActivity.this, AddEventActivity.class);
+                //intent.putExtra();
+                startActivity(intent);
+            }
+        });
     }
+
 }
---
+++ app/src/main/java/com/example/green/crmproject_android/DisplayEventsActivity.java
@@ -0,0 +1,13 @@
+package com.example.green.crmproject_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayEventsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_events);
+    }
+}
