ab3f17a74f28867ca9769b6515b8bdb03c1293d5
Genevieve Green <genevieveaileen@gmail.com>
2019-03-25 22:18:17 -0700

    Updated code, ui, and xml's for Task feature

---
+++ app/src/main/java/TaskPackage/Task.java
@@ -0,0 +1,18 @@
+package TaskPackage;
+
+public class Task {
+
+    private String desc;
+
+    public Task(String desc) {
+        this.desc = desc;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
